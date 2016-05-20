56a404309ed5553d1e2a0d32ac4ad96cf4070e90
stevenjmarsh <sjm.atl@gmail.com>
2016-05-20 15:29:31 -0400

    added a sample mocha unit test

---
+++ tests/unit/sample.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+
+import * as chai from 'chai';
+const expect = chai.expect;
+
+describe('sample', () => {
+  it('runs a sample test that passes', () => {
+    expect(1).to.equal(1);
+  });
+});
