de78c54b53a4f9047a4111011d9b73365ee38eb2
stevenjmarsh <sjm.atl@gmail.com>
2016-05-20 14:16:26 -0400

    suppressed a lint error, no-console, ok here

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// code to run on server at startup
 Meteor.startup(() => {
-  // code to run on server at startup
+  /* eslint-disable no-console */
   console.log('starting server...');
+  /* eslint-enable no-console */
 });
