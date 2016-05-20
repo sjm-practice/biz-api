65322036d833fa1e25849a9e51d413308e2adcd5
stevenjmarsh <sjm.atl@gmail.com>
2016-05-20 16:28:35 -0400

    added chimp mocha end to end simple test

---
+++ tests/end-to-end/mocha/title.js
@@ -0,0 +1,14 @@
+/* eslint-env mocha */
+/* global browser */
+
+import * as chai from 'chai';
+const expect = chai.expect;
+
+describe('Chimp Mocha', () => {
+  describe('Page Title', () => {
+    it('should be set to simple', () => {
+      browser.url('http://localhost:3000');
+      expect(browser.getTitle()).to.equal('simple');
+    });
+  });
+});
