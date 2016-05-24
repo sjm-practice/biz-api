17d347264053e867a9fedb5d466a35bb72130f51
stevenjmarsh <sjm.atl@gmail.com>
2016-05-24 13:31:42 -0400

    explicitly set div to render blaze layout template

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,6 +5,9 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layouts/app-body.js';
 import '../../ui/pages/info-page.html';
 
+// specifically set the div in body, where BlazeLayout renders
+BlazeLayout.setRoot('#layoutContainer');
+
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
