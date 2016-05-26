70d0247acd48b0b4264ff1b55357ac6efbda558f
stevenjmarsh <sjm.atl@gmail.com>
2016-05-26 18:25:05 -0400

    add simple bland, contact / send message page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import { BlazeLayout } from "meteor/kadira:blaze-layout";
 // Import to load these templates
 import "../../ui/layouts/app-body.js";
 import "../../ui/pages/info-page.html";
+import "../../ui/pages/support-page.html";
 
 // specifically set the div in body, where BlazeLayout renders
 BlazeLayout.setRoot("#layoutContainer");
@@ -12,5 +13,12 @@ FlowRouter.route("/", {
   name: "App.home",
   action() {
     BlazeLayout.render("App_body", { main: "Info_page" });
+  }
+});
+
+FlowRouter.route("/support", {
+  name: "App.support",
+  action() {
+    BlazeLayout.render("App_body", { main: "Support_page" });
   }
 });
